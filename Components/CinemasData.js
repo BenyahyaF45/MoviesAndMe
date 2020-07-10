af4a58c8b4f2fc9c53db4d629152ede5329003c7
BenyahyaF45 <BENYAHYA.FATIMAZAHRA@emsi-edu.ma>
2020-07-10 05:02:44 +0100

    Movies

---
+++ Components/CinemasData.js
@@ -0,0 +1,51 @@
+export default data = [
+    {
+        conrdinate:{
+            latitude: 34.684179,
+            longitude: -1.890164,
+        },
+        title: "Cinema Rif"
+    },
+    {
+        conrdinate:{
+            latitude: 34.688915,
+            longitude: -1.911794,
+        },
+        title: "Film Event Consulting"
+    },
+    {
+        conrdinate:{
+            latitude: 34.688870,
+            longitude: -1.917709,
+        },
+        title: "ciné nouvelle"
+    },
+    {
+        conrdinate:{
+            latitude: 34.682668,
+            longitude: -1.914576,
+        },
+        title: "Cinéma Royale"
+    },
+    {
+        conrdinate:{
+            latitude:34.678053,
+            longitude: -1.913675,
+        },
+        title: "Cinema Maghreb"
+    },
+    {
+        conrdinate:{
+            latitude: 34.682815,
+            longitude: -1.907512,
+        },
+        title: "Cinema mirage Bazard Mirage Esseddiqui"
+    },
+    {
+        conrdinate:{
+            latitude:  34.709636,
+            longitude: -1.920428,
+        },
+        title: "Cinema Siham"
+    }
+  ]
